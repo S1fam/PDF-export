400b577b2378e96ab9ad6bdcd4c1387b0323271b
jerxy <jerxylol@gmail.com>
2023-05-27 01:35:17 +0200

    Adding pages automatically with data from csv

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")  # P-portray, L-Landscape
 df = pd.read_csv("topics.csv")
 
 
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=20)  # set cells font, mm dont apply for font
+    pdf.set_text_color(69, 69, 69)
+    pdf.cell(w=60, h=12, txt="Pdf generation:", align="R", ln=0, border=0)
+    pdf.cell(w=70, h=12, txt=f"{row['Order']}: {row['Topic']}", align="R", ln=0, border=0)
+    pdf.cell(w=65, h=12, txt="- study materials", align="L", ln=1, border=0)  # basically adding text through cells
+    pdf.line(x1=10, y1=25, x2=200, y2=25)
+
+#  # iterating over items
+##
+# #
+#
+#
+#
+# #
+##
+#  # one page at the time
+
 pdf.add_page()
 
 pdf.set_font(family="Times", style="B", size=12)  # set cells font, mm dont apply for font
